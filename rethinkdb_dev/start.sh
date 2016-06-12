3d7a45535157811ee92946349f554848f890a39e
Johnny Magrippis <j@magrippis.com>
2016-06-12 11:08:59 +0100

    Tweaks the rethinkdb startup script

--- rethinkdb_dev/start.sh
+++ rethinkdb_dev/start.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+RUNNING=$(docker inspect --format="{{ .State.Running }}" yogo-rethink 2> /dev/null)
 
-echo "looking at $PWD"
+if [ $? -eq 1 ]; then
+  echo "Creating container for RethinkDB..."
+  docker run --name yogo-rethink -p 8888:8080 -p 28015:28015 -p 29015:29015 \
+    -v "$PWD/rethinkdb_dev/data:/data/rethinkdb_data" \
+    -d rethinkdb rethinkdb --bind all -n yogofrozen \
+    && echo "Createad and started."
+  exit 0
+fi
 
-echo "Starting rethinkdb..."
-docker run --name yogo-rethink -p 8888:8080 -p 28015:28015 -p 29015:29015 \
-  -v "$PWD:/rethinkdb_dev/data" \
-  -d rethinkdb rethinkdb --bind all -n yogofrozen \
-  && echo "Started."
+if [ "$RUNNING" == "false" ]; then
+  echo "Starting RethinkDB."
+  docker start yogo-rethink
+  exit 0
+fi
+
+echo "Dockerized RethinkDB for Yogo Frozen already running!"
