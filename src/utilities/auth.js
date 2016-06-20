35148b2523809eb2fd30602416c3d92f88492a00
Johnny Magrippis <j@magrippis.com>
2016-06-20 07:33:36 +0100

    Properly sets the window location for the hardcoded redirect.

--- src/utilities/auth.js
+++ src/utilities/auth.js
@@ -1,9 +1,9 @@
 export const login = (horizon, provider) => {
   if (provider === 'facebook') {
-    window.location.pathname = 'https://yogofrozen.gr:8181/horizon/facebook'
+    window.location.assign('https://yogofrozen.gr:8181/horizon/facebook')
     return
   }
   horizon.authEndpoint(provider).subscribe((endpoint) => {
-    window.location.pathname = 'https://yogofrozen.gr:8181' + endpoint
+    window.location.assign('https://yogofrozen.gr:8181' + endpoint)
   })
 }
