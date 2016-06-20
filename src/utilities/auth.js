51450dd43df5b2117c3af0a83b61a7a05f64a0dd
Johnny Magrippis <j@magrippis.com>
2016-06-20 02:09:39 +0100

    Hardcoded Facebook redirect due to Horizon limitations

--- src/utilities/auth.js
+++ src/utilities/auth.js
@@ -1,4 +1,8 @@
 export const login = (horizon, provider) => {
+  if (provider === 'facebook') {
+    window.location.pathname = 'https://yogofrozen.gr:8181/horizon/facebook'
+    return
+  }
   horizon.authEndpoint(provider).subscribe((endpoint) => {
     window.location.pathname = 'https://yogofrozen.gr:8181' + endpoint
   })
